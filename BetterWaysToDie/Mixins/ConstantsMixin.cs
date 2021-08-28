b38d2658f03f0a57d6c34b56183ff2c27a59a79f
Ramid Khan <ramidzkh@gmail.com>
2021-08-28 10:13:41 +1000

    Works on my machine

--- BetterWaysToDie/Mixins/ConstantsMixin.cs
+++ BetterWaysToDie/Mixins/ConstantsMixin.cs
@@ -2,14 +2,17 @@
 using SharpILMixins.Annotations.Inject;
 using SharpILMixins.Annotations.Inline;
 
-namespace BetterWaysToDie.Mixins {
+namespace BetterWaysToDie.Mixins
+{
     [Mixin(typeof(Constants))]
-    public class ConstantsMixin {
+    public class ConstantsMixin
+    {
         [Shadow] private static string cVersion;
 
         [Inject(".cctor", AtLocation.Tail)]
         [Inline]
-        private static void Crab() {
+        private static void Crab()
+        {
             cVersion = "Blender Fortress 2.82";
         }
     }
